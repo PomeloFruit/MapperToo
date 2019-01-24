7949c24d6ba5f5bf57b978d20d161b425b41340f
Ian Sartor <ian.sartor@mail.utoronto.ca>
2019-01-23 20:23:43 -0500

    Working 4th function. Speed of 3rd function improved

--- libstreetmap/src/m1.cpp
+++ libstreetmap/src/m1.cpp
@@ -43,95 +43,110 @@ bool load_map(std::string path/*map_path*/) {
 }
 
 void close_map() {
+
+    //so according to this I just don't have the right street segments for some of these
     closeStreetDatabase();
     //Clean-up your map related data structures here
     
 }
 
+std::vector<unsigned> find_intersection_street_segments(unsigned intersection_id){
+    std::vector<unsigned> ids;
+    int numOfSegs=getIntersectionStreetSegmentCount(intersection_id);
+    for(int i=0;i<numOfSegs;i++){
+        ids.push_back(getIntersectionStreetSegment(i, intersection_id));
+    }
+    return ids;
+    //so as of now this or any of the other things I write won't pass the performance test
+    //but don't worry I have it all under control just like these versions
+    //to fix this one and probably all the other ones I'm about to write 
+    //create a global variable and make a nested for loops for nested vectors
+           
+}
 
-//
-//std::vector<unsigned> find_intersection_street_segments(unsigned intersection_id){
-//    std::vector<unsigned> ids;
-//    int numOfSegs=getIntersectionStreetSegmentCount(intersection_id);
-//    for(int i=0;i<numOfSegs;i++){
-//        ids.push_back(getIntersectionStreetSegment(i, intersection_id));
-//    }
-//    return ids;
-//    //so as of now this or any of the other things I write won't pass the performance test
-//    //but don't worry I have it all under control just like these versions
-//    //to fix this one and probably all the other ones I'm about to write 
-//    //create a global variable and make a nested for loops for nested vectors
-//            
-//}
-//
-//std::vector<std::string> find_intersection_street_names(unsigned intersection_id){
-//    //this is supposed to return duplicate names here also so don't worry about
-//    std::vector<std::string> names;
-//    int numOfSegs=getIntersectionStreetSegmentCount(intersection_id);
-//    for(int i=0;i<numOfSegs;i++){
-//        names.push_back(getStreetName(getIntersectionStreetSegment(i, intersection_id))); //It's gross but it works I think
-//    }
-//    return names;
-//    //this will suffer from the same problems as the prev function
-//}
-//
-//bool are_directly_connected(unsigned intersection_id1, unsigned intersection_id2){
-//    //so the corner case here is if it has curvepoints or not I think??
-//    //but basically do they share a road and if so is the road one way
-//    //is an intersection always connected to itself? or does a street seg need to curve back into itself?
-//    //**read above unless I get answers later
-//    //if an intersection is always connected to itself I can just return true if they are =
-//    //and finally this would be easier if I had made the global variable first but oh well
-//    //bool returniee=false;
-//    std::vector<unsigned> segsInt1=find_intersection_street_segments(intersection_id1);
-//    std::vector<unsigned> segsInt2=find_intersection_street_segments(intersection_id2);
-//    //so I have the two lists
-//    //literally O(n^2) incoming
-//    //if you have a better idea please implement it
-//    for(int i=0;i<segsInt1.size();i++){
-//        for(int c=0;c<segsInt2.size();c++){
-//            if(segsInt1[i]==segsInt2[c]){
-//                if(!getInfoStreetSegment(segsInt1[i]).oneWay){
-//                    return true;
-//                }
-//                else if(getInfoStreetSegment(segsInt1[i]).oneWay&&(getInfoStreetSegment(segsInt1[i]).from==intersection_id1)){
-//                    return true;
-//                }
-//            }
-//        }
-//    }
-//    
-//    return false;
-//    //this is really not going to pass the speed test
-//}
-//
-//std::vector<unsigned> find_adjacent_intersections(unsigned intersection_id){
-//    //no duplicates allowed
-//    //another computationally intense thing
-//    //this could be fixed using a hash table though
-//    //but I'm too lazy to fix it right now so I'll write down the solution for the future
-//    //just make a hash table and hash each connectedIntersection as I find it
-//    //if there is a collision there must be a duplicate and I won't add the thing to the list ezpz
-//    //but for now I'm just gonna O(n^2) this
-//    std::vector<unsigned> segsOrigin=find_intersection_street_segments(intersection_id);
-//    std::vector<unsigned> connectedIntersections;
-//    bool insert;
-//    for(int i=0;i<segsOrigin.size();i++){
-//        insert=true;
-//        for(int c=0;c<connectedIntersections.size();c++){
-//            if(connectedIntersections[c]==getInfoStreetSegment(segsOrigin[i]).to){
-//                insert=false;
-//            }
-//            //numInVec=std::count(connectedIntersecitons.begin(), connectedIntersecitons.end(), target1);
-//            //on the bright side I get to look at more STL stuff but I don't know if I'm allowed to use the algos there
-//        }
-//        if(insert){
-//            connectedIntersections.push_back(getInfoStreetSegment(segsOrigin[i]).to);
-//        }
-//    }
-//    return connectedIntersections;
-//    
-//}
+std::vector<std::string> find_intersection_street_names(unsigned intersection_id){
+    //this is supposed to return duplicate names here also so don't worry about
+    std::vector<std::string> names;
+    int numOfSegs=getIntersectionStreetSegmentCount(intersection_id);
+    for(int i=0;i<numOfSegs;i++){
+        names.push_back(getStreetName(getIntersectionStreetSegment(i, intersection_id))); //It's gross but it works I think
+    }
+    return names;
+    //this will suffer from the same problems as the prev function
+}
+
+bool are_directly_connected(unsigned intersection_id1, unsigned intersection_id2){
+    //so the corner case here is if it has curvepoints or not I think??
+    //but basically do they share a road and if so is the road one way
+    //is an intersection always connected to itself? or does a street seg need to curve back into itself?
+    //**read above unless I get answers later
+    //if an intersection is always connected to itself I can just return true if they are =
+    //and finally this would be easier if I had made the global variable first but oh well
+    //NOW O(n)
+    std::vector<unsigned> segsInt1=find_intersection_street_segments(intersection_id1);
+
+    for(unsigned i=0;i<segsInt1.size();i++){
+        if((unsigned(getInfoStreetSegment(segsInt1[i]).to)==intersection_id2)){
+            return true;
+        }
+        else if((unsigned(getInfoStreetSegment(segsInt1[i]).to)==intersection_id1)&&(unsigned(getInfoStreetSegment(segsInt1[i]).from)==intersection_id2)){
+            if(!getInfoStreetSegment(segsInt1[i]).oneWay){
+                return true;
+            }
+        }
+    }
+    //I'm dumb
+    //they are directly connected if there is a segment between them (only check one of them)
+    //and if the segment is one way it is only invalid if to is id1
+    //if to is intersection to return true
+    
+    return false;
+    //this is really not going to pass the speed test
+}
+
+std::vector<unsigned> find_adjacent_intersections(unsigned intersection_id){
+    //no duplicates allowed
+    //another computationally intense thing
+    //this could be fixed using a hash table though
+    //but I'm too lazy to fix it right now so I'll write down the solution for the future
+    //just make a hash table and hash each connectedIntersection as I find it
+    //if there is a collision there must be a duplicate and I won't add the thing to the list ezpz
+    //but for now I'm just gonna O(n^2) this
+    //a street cannot be adjacent to itself
+    std::vector<unsigned> segsOrigin=find_intersection_street_segments(intersection_id);
+    std::vector<unsigned> connectedIntersections;
+    bool insert;
+    bool invalidTo;
+    bool invalidFrom;
+    for(unsigned i=0;i<segsOrigin.size();i++){
+        insert=true;
+        invalidTo=false;
+        invalidFrom=false;
+        for(unsigned c=0;c<connectedIntersections.size();c++){
+            if((connectedIntersections[c]==unsigned(getInfoStreetSegment(segsOrigin[i]).to)||(unsigned(getInfoStreetSegment(segsOrigin[i]).to)==intersection_id))){
+                invalidTo=true;
+            }
+            if((connectedIntersections[c]==unsigned(getInfoStreetSegment(segsOrigin[i]).from))||(unsigned(getInfoStreetSegment(segsOrigin[i]).from)==intersection_id)){
+                invalidFrom=true;
+            }
+        }
+        //insert=true;
+        if((getInfoStreetSegment(segsOrigin[i]).oneWay)&&(unsigned(getInfoStreetSegment(segsOrigin[i]).to)==intersection_id)){
+            insert=false;
+        }
+        if(insert){
+            if((unsigned(getInfoStreetSegment(segsOrigin[i]).to)!=intersection_id)&&!invalidTo){
+                connectedIntersections.push_back(getInfoStreetSegment(segsOrigin[i]).to);
+            }
+            else if(!invalidFrom){
+                connectedIntersections.push_back(getInfoStreetSegment(segsOrigin[i]).from);
+            }
+        }
+    }
+    return connectedIntersections;
+    //if a way is found to make the is directly connected function faster we could just call it here
+    //or we could call this in the place of is directly connected maybe
+}
 
 //so this again won't pass speed tests but I know a solution exists (and outlined above)
 //ALSO ONE IMPORTANT NOTE, NONE OF THESE FUNCTIONS WERE TESTED BECAUSE PEOPLE WERE HAVING PROBLEMS DOING IT
