27117a2613b3841363fd2f7305cff8d0ec8faf53
Ian Sartor <ian.sartor@mail.utoronto.ca>
2019-02-17 22:21:45 -0500

    I played around with including the globals from m1 in m2

---
+++ libstreetmap/src/globals.h
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   globals.h
+ * Author: sartori2
+ *
+ * Created on February 17, 2019, 5:44 AM
+ */
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+//unordered map of <street ids, street segments with the same street ids>
+#include <vector>
+#include <string>
+extern std::unordered_map<int, std::vector<unsigned>> streetIDMap; 
+
+//map of <partial street name, street ids with the same partial street name>
+//(ie. the name Bloor becomes five vectors: b, bl, blo, bloo, bloor)
+extern std::map<std::string, std::vector<unsigned>> partialStreetNameMap;
+
+//vector of vectors of intersections on a street 
+extern std::vector<std::vector<unsigned>> streetIntersectionsVector; 
+
+//vector of street segment ID indexed on intersection ID
+extern std::vector<std::vector<unsigned>> intersectionSegIDVector;
+
+//vector of street segment names indexed on intersection Id
+extern std::vector<std::vector<std::string>> intersectionSegNameVector;
+
+//vector of street segment lengths indexed on segment ID
+extern std::vector<double> segLengthVector;
+
+//vector of street lengths indexed on street ID
+extern std::vector<double> streetLengthVector;
+
+//vector of street segment travel times indexed on segment ID
+extern std::vector<double> segTravelTimeVector;
+
+
+
+#endif /* GLOBALS_H */
+
--- libstreetmap/src/m2.cpp
+++ libstreetmap/src/m2.cpp
@@ -4,3 +4,39 @@
  * and open the template in the editor.
  */
 
+#include "m1.h"
+#include "m2.h"
+#include "globals.h"
+#include "StreetsDatabaseAPI.h"
+#include <map>
+#include <unordered_map>
+#include <math.h>
+#include <algorithm>
+#include <string>
+#include <iostream>
+void draw_map(){
+
+}
+
+//Somewhere in a big function(load_map like-thing) we should define & make global some try to make constant the adjustment factor (latmin+latmax)/2
+std::pair<double, double> Lat_Lon_To_X_Y(LatLon point){
+    //double pt1LatInRad = point1.lat()*DEG_TO_RAD;
+    //double pt2LatInRad = point2.lat()*DEG_TO_RAD;
+    //double averageLatInRad = (pt1LatInRad+pt2LatInRad)/2.0; to be defined in another function
+    double projectionFactor = cos(averageLatInRad);
+    double x=point.lon()*projectionFactor;
+    double y=point.lat();
+    return make_pair(x,y);
+}
+double Average_Lat(){
+    //for this function I assume I already have all the intersections loaded
+    //to do this I'd need to make a global variables.h thing
+    //and since I don't want to do that without telling anyone and you are all
+    //probably asleep I simply will not make one
+    double maxLat=getIntersectionPosition(1).lat();
+    double minLat=maxLat;;
+    for(int i=0;i<){
+    
+    }
+    return (maxLat+minLat)/2;
+}
