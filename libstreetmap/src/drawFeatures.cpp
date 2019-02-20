0b43ada2f485484050a2e775c3595f4e155b3ad4
Alan Du <alan.du@mail.utoronto.ca>
2019-02-20 17:09:47 -0500

    updated all the existing colours to that of the specifications in WD1 updated the POI colouring so that it displays different types of POIs with different colours
    *problem* even though some POIs are not drawn, they still exist and the
    "findnearestPOI" function still finds it

--- libstreetmap/src/drawFeatures.cpp
+++ libstreetmap/src/drawFeatures.cpp
@@ -5,6 +5,19 @@
 #include "LatLon.h"
 #include "ezgl/graphics.hpp"
 
+#include <algorithm>
+
+std::vector<std::string> tourist {"aquarium", "artwork", "attraction", "gallery", "information", "museum", "theme_park", "zoo", "college", "university", "brothel", "arts_centre"
+    , "casino", "cinema", "fountain", "gambling", "music_venue", "nightclub", "planetarium", "theatre", "marketplace", "townhall", "stadium"};
+    
+std::vector<std::string> foodDrink {"alcohol", "bakery", "beverages", "coffee", "confectionery", "convenience", "ice_cream","pastry", "seafood", "bar", "bbq", "cafe", 
+    "food_court", "pub", "restaurant"}; 
+    //note: i don't have "fast_food" in here because there are too many foodDrink POIs
+std::vector<std::string> shops {"department_store", "general", "kiosk", "mall", "supermarket", "wholesale", "bag", "boutique", "clothes" , "jewelry", "leather"
+    , "shoes", "watches", "variety", "second_hand", "beauty", "cosmetics", "erotic", "hairdresser", "herbalist", "massage", "medical_supply", "perfumery"
+    , "tattoo", "electrical", "florist", "antiques", "candles", "interior_decoration", "computer", "robot", "electronics", "mobile_phone", "radiotechnics" 
+    , "fishing", "fuel", "outdoor", "scuba_diving", "ski", "sports", "swimming_pool", "art", "collector", "craft", "games", "model", "music", "musical_instrument"
+    , "photo", "camera", "video", "video_games", "anime", "books", "gift", "stationery", "ticket", "cannabis", "e-cigarette", "tobacco", "toys", "travel_agency"};
 
 void featureDrawing::setFeatureColour(int type, ezgl::renderer &g){
     switch(type){
@@ -12,38 +25,68 @@ void featureDrawing::setFeatureColour(int type, ezgl::renderer &g){
             g.set_color(152,151,150,255);
             break;
         case 1: // park = darkish green
-            g.set_color(92,201,103,255);
+            g.set_color(85, 209, 49, 79);
             break;
         case 2: // beach = peach
             g.set_color(235,210,111,255);
             break;
         case 3: // lake = blue 
-            g.set_color(16,184,225,255);
+            g.set_color(60, 182, 255, 163);
+            //g.set_color(16,184,225,255);
             break;
         case 4: // river = dark blue 
-            g.set_color(0,120,149,255);
+            g.set_color(80, 179, 255, 128);
             break;
         case 5: // island = dark green
-            g.set_color(53,92,17,255);
+            g.set_color(100,209, 0, 99);
             break;
         case 6: // building = darkish gray
-            g.set_color(127,129,125,255);
+            g.set_color(22, 23, 56, 20);
             break;
         case 7: // green space = light green 
-            g.set_color(95,218,24,255);
+            g.set_color(82, 204, 42, 56);
+            //g.set_color(95,218,24,255);
             break;
         case 8: // golf course = putting green
-            g.set_color(161,23,115,225);
+            g.set_color(148,235,87,155);
             break;
         case 9: // stream = light blue
-            g.set_color(115,226,234,255);
+            g.set_color(80, 179, 255, 128);
             break;
         default:
             g.set_color(152,151,150,255);
             break;
     }
 }
-        
+ 
+
+int featureDrawing::classifyPOI(std::string type, ezgl::renderer& g){
+    int poiType = 0; 
+    if(std::find(tourist.begin(), tourist.end(), type) != tourist.end()){
+        poiType = 1; 
+    }
+    if(std::find(foodDrink.begin(), foodDrink.end(), type) != foodDrink.end()){
+        poiType = 2;
+    }
+    if(std::find(shops.begin(), shops.end(), type) != shops.end()){
+        poiType = 3; 
+    }
+    return poiType; 
+}
+
+void featureDrawing::setPOIColour(int type, ezgl::renderer& g){
+    if(type == 1){
+        g.set_color(255,71,113,191);
+    }else if (type == 2){
+        g.set_color(255, 165, 54, 191);
+    }else if (type == 3){
+        g.set_color(0, 112, 255, 191);
+    }
+    else{
+        g.set_color(0,0,0,0);
+    }
+}
+
 
 void featureDrawing::drawFeatures(int numFeatures, infoStrucs &info, ezgl::renderer &g){
     for(int i=0 ; i<numFeatures ; i++){
@@ -66,7 +109,7 @@ void featureDrawing::drawFeatures(int numFeatures, infoStrucs &info, ezgl::rende
 
 // draw all, then make sure last clicked is on top of others
 void featureDrawing::drawPOI(int numPOI, mapBoundary &xy, infoStrucs &info, ezgl::renderer &g){
-    for(int i=0 ; i<numPOI ; i++){
+    for(int i=0 ; i<numPOI ; i++){   
         drawOnePOI(i, xy, info, g);
     }
     drawOnePOI(info.lastPOI, xy, info, g);
@@ -84,15 +127,19 @@ void featureDrawing::drawOnePOI(int i, mapBoundary &xy, infoStrucs &info, ezgl::
     yNew = xy.yFromLat(newPoint.lat());
     
     if (info.POIInfo[i].clicked) {
-        radius = NORMALRAD;
-        g.set_color(0,255,174,255);
-        g.fill_elliptic_arc(ezgl::point2d(xNew,yNew),radius,radius,0,360);
+        //highlight circle (pink)
         radius = HIGHLIGHTRAD;
-        g.set_color(214,115,246,100);
+        g.set_color(255,77,190,125);
         g.fill_elliptic_arc(ezgl::point2d(xNew,yNew),radius,radius,0,360);
-    } else {
+        //inner circle (dark purple)
         radius = NORMALRAD;
-        g.set_color(255,0,0,255);
+        g.set_color(79,0,79,255);
+        g.fill_elliptic_arc(ezgl::point2d(xNew,yNew),radius,radius,0,360);
+
+    } else {
+        //regular POI (dark red)
+        radius = NORMALRAD;
+        setPOIColour(classifyPOI(info.POIInfo[i].type, g), g); 
         g.fill_elliptic_arc(ezgl::point2d(xNew,yNew),radius,radius,0,360);
     }
 }
